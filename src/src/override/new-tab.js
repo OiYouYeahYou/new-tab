bfa5045ef2ba9974a60a5216f4f66951c270c2ab
Jason <code@jasonderrythomas.com>
2020-01-29 19:53:55 +0000

    Add basic overview of tabs

--- src/src/override/new-tab.js
+++ src/src/override/new-tab.js
@@ -1,9 +1,10 @@
 let domLoaded = false
 let imageLoaded = false
 const { innerWidth, innerHeight } = window
-const img = document.createElement('img')
+const img = image(
+	`https://source.unsplash.com/random/${innerWidth}x${innerHeight}`
+)
 img.addEventListener('load', contentLoaded)
-img.src = `https://source.unsplash.com/random/${innerWidth}x${innerHeight}`
 
 window.addEventListener('DOMContentLoaded', domLoad)
 
@@ -21,4 +22,22 @@ function appendImage() {
 	if (domLoaded && imageLoaded) {
 		document.body.appendChild(img)
 	}
+}
+
+chrome.windows.getCurrent({ populate: true }, function(window) {
+	const { tabs } = window
+	let list = document.getElementById('tab-list')
+	tabs.forEach(tab => {
+		const item = document.createElement('li')
+		item.appendChild(image(tab.favIconUrl))
+		item.appendChild(document.createTextNode(tab.title))
+		list.appendChild(item)
+	})
+})
+
+function image(url) {
+	const img = document.createElement('img')
+	img.src = url
+	img.referrerPolicy = 'no-referer'
+	return img
 }
